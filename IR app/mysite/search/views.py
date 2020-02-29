3307af2852f9290f85ccf663c1edd04baa519c7a
Germaine Kwek <danyi1998@gmail.com>
2020-03-01 02:54:41 +0800

    changed the views file

--- IR app/mysite/search/views.py
+++ IR app/mysite/search/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+import requests 
 
 def home(request):
     query = request.POST.get("query_search") 
-    print(query) 
+    if type(query)==str:
+        response = requests.get('http://localhost:8983/solr/techproducts/select?q=' + query)
+        response = response.json() 
+        for i in range(0):
+            print(response["response"]["docs"][i]["name"]) 
+    
     return render(request, "search/home.html")
 
 def results(request):
