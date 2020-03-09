fda13f52aada526e8f1e95a235484a936b735c38
kevinyx2 <48469266+kevinyx2@users.noreply.github.com>
2020-03-09 16:39:42 +0800

    Update lemma.py

--- lemma.py
+++ lemma.py
@@ -1,6 +1,9 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+from nltk import word_tokenize
+import string
 
 
 class lemmatization(object):
@@ -27,3 +30,28 @@ class lemmatization(object):
         for item in lst:
             lemmatized_list.append(self.lmtzr.lemmatize(item))
         return lemmatized_list
+    
+    def tokenize(text):
+
+        #Create Stemmer
+        stemmer = PorterStemmer()
+
+        #Remove irrelevant character
+        text = re.sub(r"http\S+", '', text)
+        text = re.sub(r"[^a-zA-Z]", ' ', text)
+
+        #Tokenization
+        tokens = word_tokenize(text)
+        tokens = [i for i in tokens if i not in string.punctuation]
+
+        #Stemming
+        stems = stem_tokens(tokens, stemmer)
+        return stems
+
+    #Stemming Function
+    def stem_tokens(t,s):
+        stemmed=[]
+        for item in t:
+            stemmed.append(s.stem(item))
+        return stemmed
+
